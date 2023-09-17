b00b304ec49b86a33e8156b48a1a674f0ae61509
miyou995 <inter.taki@gmail.com>
2023-09-17 16:13:33 +0100

    hen

--- core/views.py
+++ core/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
+from django.utils.translation import *
 
-# Create your views here.
+
 def index_view(request):
-    return render(request,'index.html')
+    template_name =  'rtl/index.html' if request.LANGUAGE_CODE == 'ar' else 'index.html'
+    return render(request,template_name)
 
-# Create your views here.
+
 def about(request):
-    return render(request,'about.html')
+    template_name =  'rtl/about.html' if request.LANGUAGE_CODE == 'ar' else 'about.html'
+    return render(request,template_name)
 
 def contact(request):
-    return render(request,'about.html')
-
+    template_name =  'rtl/contact.html' if request.LANGUAGE_CODE == 'ar' else 'contact.html'
+    return render(request,template_name)
 
 def services(request):
-    return render(request,'about.html')
+    template_name =  'rtl/services.html' if request.LANGUAGE_CODE == 'ar' else 'services.html'
+    return render(request,template_name)
 
 def hiring(request):
-    return render(request,'about.html')
+    template_name =  'rtl/hiring.html' if request.LANGUAGE_CODE == 'ar' else 'hiring.html'
+    return render(request,template_name)
+
